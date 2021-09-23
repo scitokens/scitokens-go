b305a7843e19e43de81b3ef77088c557b501be38
Kevin Retzke <kretzke@fnal.gov>
2021-09-23 16:27:43 -0500

    scitoken-validate: add group flag

--- cmd/scitoken-validate/main.go
+++ cmd/scitoken-validate/main.go
@@ -15,6 +15,8 @@ var (
 		"trusted token issuer for obtaining keys and validating token. Can be repeated.")
 	scopes *[]string = flag.StringSliceP("scope", "s", []string{},
 		"scope to validate, with optional path delimited by colon. Can be repeated.")
+	groups *[]string = flag.StringSliceP("group", "g", []string{},
+		"WLCG group to validate. The leading slash on the group name is optional. Can be repeated.")
 	verbose *bool = flag.BoolP("verbose", "v", false,
 		"extra logging of token information and internals to stderr.")
 )
@@ -31,7 +33,11 @@ At least one token issuer must be specified with the --issuer/-i flag, which can
 be repeated. Signing keys will be fetched for each issuer to validate the tokens
 signature, and it will also be validated that the token was issued by one of them.
 
-Optionally pass one or more scopes to validate with the --scope/-s flag.
+Optionally pass one or more scopes to validate with the --scope/-s flag. Note that
+scope paths will validate sub-paths (see example).
+
+Optionally pass one or more WLCG groups to require with the --group/-g flag. Note
+that groups WILL NOT validate sub-groups (see example).
 
 If the token is not valid an explanation message will be printed to stderr and
 the program will terminate with exit code 1.
@@ -44,8 +50,14 @@ Basic validation:
 Enforce scopes:
     scitoken-validate -i https://cilogon.org/fermilab -s compute.create -s storage.read:/fermilab/users/kretzke/foo
 
-(Note that scope paths will validate sub-paths, e.g. this will validate a
-token that has storage.read:/fermilab/users/kretzke)
+    Note that scope paths will validate sub-paths, e.g. this will validate a
+    token that has storage.read:/fermilab/users/kretzke.
+
+Enforce groups:
+    scitoken-validate -i https://cilogon.org/fermilab -g nova/production
+
+    Note that groups WILL NOT validate sub-groups, e.g. this will NOT validate a
+    token that only has "/nova".
 
 FLAGS
 `)
@@ -65,6 +77,11 @@ func main() {
 	for _, s := range *scopes {
 		if err = enf.RequireScope(scitokens.ParseScope(s)); err != nil {
 			log.Fatalf("unable to require scope %s: %s", s, err)
+		}
+	}
+	for _, g := range *groups {
+		if err = enf.RequireGroup(g); err != nil {
+			log.Fatalf("unable to require group %s: %s", g, err)
 		}
 	}
 
