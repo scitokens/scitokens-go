9dc01740ff07e6140b17b4267eeceb3cfce97377
Kevin Retzke <kretzke@fnal.gov>
2021-09-29 08:04:05 -0500

    key: check openid-configuration suffix. fixes #1
    Well-known suffixes to check are put in package variable, to allow
    clients to add others if needed.

--- key.go
+++ key.go
@@ -11,6 +11,17 @@ import (
 	"github.com/lestrrat-go/jwx/jwk"
 )
 
+var (
+	// WellKnown is a list of well-known URL suffixes to check for OAuth server
+	// metadata. See
+	// https://www.iana.org/assignments/well-known-uris/well-known-uris.xhtml
+	// https://datatracker.ietf.org/doc/html/draft-ietf-oauth-discovery-07
+	WellKnown = []string{
+		"oauth-authorization-server",
+		"openid-configuration",
+	}
+)
+
 // AuthServerMetadata per
 // https://datatracker.ietf.org/doc/html/draft-ietf-oauth-discovery-07. Fields
 // defined as OPTIONAL that aren't currently used are not included.
@@ -25,6 +36,12 @@ type AuthServerMetadata struct {
 	ResponseTypes   []string `json:"response_types_supported"`
 }
 
+type NotFoundError struct{}
+
+func (e NotFoundError) Error() string {
+	return "404 not found"
+}
+
 // FetchMetadata retrieves the OAUTH 2.0 authorization server metadata from the
 // given URL, which must include the complete well-known path to the resource.
 func FetchMetadata(ctx context.Context, urlstring string) (*AuthServerMetadata, error) {
@@ -37,7 +54,10 @@ func FetchMetadata(ctx context.Context, urlstring string) (*AuthServerMetadata, 
 		return nil, err
 	}
 	defer r.Body.Close()
-	if r.StatusCode != 200 {
+	switch {
+	case r.StatusCode == http.StatusNotFound:
+		return nil, NotFoundError{}
+	case r.StatusCode != 400:
 		return nil, fmt.Errorf("%s", r.Status)
 	}
 
@@ -57,12 +77,24 @@ func FetchMetadata(ctx context.Context, urlstring string) (*AuthServerMetadata, 
 // GetIssuerKeys retrieves all JSON Web Keys for the given issuer, as specified
 // by the issuer's metadata, which is expected to be at the issuer URL under
 // .well-known/oauth-authorization-server.
-// TODO: maybe try other well-known suffixes?
 // TODO: use https://pkg.go.dev/github.com/lestrrat-go/jwx/jwk#AutoRefresh
 func GetIssuerKeys(ctx context.Context, issuer string) (jwk.Set, error) {
-	meta, err := FetchMetadata(ctx, issuer+path.Join("/.well-known", "oauth-authorization-server"))
-	if err != nil {
-		return nil, err
+	var meta *AuthServerMetadata
+	for _, wk := range WellKnown {
+		var err error
+		meta, err = FetchMetadata(ctx, issuer+path.Join("/.well-known", wk))
+		if err == nil {
+			break
+		} else {
+			switch err.(type) {
+			case NotFoundError:
+				continue
+			}
+			return nil, fmt.Errorf("status code %s when fetching metadata for %s", err, issuer)
+		}
+	}
+	if meta == nil {
+		return nil, fmt.Errorf("no server metadata found under %s", issuer)
 	}
 	return jwk.Fetch(ctx, meta.JWKSURL)
 }
