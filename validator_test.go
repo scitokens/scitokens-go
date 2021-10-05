81a6bab9c6013f4372e15e6b894b10c710162b86
Kevin Retzke <kretzke@fnal.gov>
2021-10-05 09:58:38 -0500

    test group validator

--- validator_test.go
+++ validator_test.go
@@ -13,7 +13,7 @@ func TestScopeValidator(t *testing.T) {
 	ctx := context.Background()
 
 	t1 := jwt.New()
-	t1.Set("scope", "read:/foo")
+	t1.Set("scope", "read:/foo write:/foo/bar")
 	st1, err := NewSciToken(t1)
 	if !assert.NoError(err) {
 		return
@@ -23,15 +23,46 @@ func TestScopeValidator(t *testing.T) {
 	assert.Error(v.Validate(ctx, t1), "cannot validate non-SciToken")
 	assert.NoError(v.Validate(ctx, st1), "token has read permission on /foo")
 
-	v = WithScope(Scope{"read", "/"})
-	assert.Error(v.Validate(ctx, st1), "token does not have read permission on /")
+	v = WithScope(Scope{"read", "/foo/bar"})
+	assert.NoError(v.Validate(ctx, st1), "sub-paths can be read")
 
-	v = WithScope(Scope{"read", "/bar"})
-	assert.Error(v.Validate(ctx, st1), "token does not have read permission on /bar")
+	v = WithScope(Scope{"read", "/qux"})
+	assert.Error(v.Validate(ctx, st1), "token does not have read permission on /qux")
 
 	v = WithScope(Scope{"write", "/foo"})
 	assert.Error(v.Validate(ctx, st1), "token does not have write permission on /foo")
 
+	v = WithScope(Scope{"write", "/foo/bar"})
+	assert.NoError(v.Validate(ctx, st1), "token has write permission on /foo/bar")
+
+	v = WithScope(Scope{"write", "/foo/bar/baz"})
+	assert.NoError(v.Validate(ctx, st1), "sub-paths can be written")
+
 	v = WithScope(Scope{"compute", ""})
 	assert.Error(v.Validate(ctx, st1), "token does not have compute permission")
+}
+
+func TestGroupValidator(t *testing.T) {
+	assert := assert.New(t)
+	ctx := context.Background()
+
+	t1 := jwt.New()
+	t1.Set("wlcg.groups", []interface{}{"/foo"})
+	st1, err := NewSciToken(t1)
+	if !assert.NoError(err) {
+		return
+	}
+
+	v := WithGroup("/foo")
+	assert.Error(v.Validate(ctx, t1), "cannot validate non-SciToken")
+	assert.NoError(v.Validate(ctx, st1), "token has group foo")
+
+	v = WithGroup("foo")
+	assert.NoError(v.Validate(ctx, st1), "leading slash is optional")
+
+	v = WithGroup("bar")
+	assert.Error(v.Validate(ctx, st1), "token does not have group bar")
+
+	v = WithGroup("foo/bar")
+	assert.Error(v.Validate(ctx, st1), "token does not have sub-group foo/bar")
 }
